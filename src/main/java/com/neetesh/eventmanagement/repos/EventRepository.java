daa9cb993ba9e947ce1715a0856d78258cc25387
malay.c.ext@flydubai.com <neeteshsharma580@gmail.com>
2017-09-07 12:02:04 +0530

    Implement Security

--- src/main/java/com/neetesh/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/neetesh/eventmanagement/repos/EventRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.web.ProjectedPayload;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.neetesh.eventmanagement.entities.Event;
 import com.neetesh.eventmanagement.entities.projections.PartialEventProjection;
@@ -18,5 +20,9 @@ public interface EventRepository extends PagingAndSortingRepository<Event, Long>
 	Page<Event> findByName(@Param("name") String name, Pageable pageable);
 	
 	Page<Event> findByNameAndZoneId(@Param("name")String name,@Param("zoneId") ZoneId zoneId, Pageable pageable);
+	
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	void delete(Long id);
 
 }
